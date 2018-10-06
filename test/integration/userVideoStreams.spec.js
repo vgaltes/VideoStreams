f827911c1134883d231dc5591329b98979179ac7
Vicenc Garcia-Altes <vicentredrum@gmail.com>
2018-10-06 15:30:19 +0100

    Add test when the user is viewing two videos.

--- test/integration/userVideoStreams.spec.js
+++ test/integration/userVideoStreams.spec.js
@@ -36,12 +36,12 @@ function insertVideoStream(userId, videoId) {
   return dynamoDb.put(params).promise();
 }
 
-async function fillUserVideoStreams(userId) {
-  return Promise.all([
-    insertVideoStream(userId, uuidv4()),
-    insertVideoStream(userId, uuidv4()),
-    insertVideoStream(userId, uuidv4())
-  ]);
+async function fillUserVideoStreams(userId, numberOfVideos) {
+  const inserts = [];
+  for (let i = 0; i < numberOfVideos; i += 1) {
+    inserts.push(insertVideoStream(userId, uuidv4()));
+  }
+  return Promise.all(inserts);
 }
 
 describe("Given a user is not watching any video stream", () => {
@@ -74,7 +74,7 @@ describe("Given a user is already watching three video streams", () => {
     // TODO: retrieve from SSM. Probably refactor to another file.
     process.env.videoStreamsTableName = "videoStreams-dev";
 
-    await fillUserVideoStreams(userId);
+    await fillUserVideoStreams(userId, 3);
   });
 
   test("she shouldn't be able to watch a new video", async () => {
@@ -83,6 +83,32 @@ describe("Given a user is already watching three video streams", () => {
       { user_id: userId }
     );
 
+    expect(response.statusCode).toBe(403);
+    expect(response.body).toEqual({
+      message: "User watching too many video streams."
+    });
+  });
+});
+
+describe("Given a user is already watching two video streams", () => {
+  const userId = uuidv4();
+  const videoId = uuidv4();
+
+  beforeAll(async () => {
+    // TODO: retrieve from SSM. Probably refactor to another file.
+    process.env.videoStreamsTableName = "videoStreams-dev";
+
+    await fillUserVideoStreams(userId, 2);
+  });
+
+  test("she should be able to watch a new video, but not another one", async () => {
+    let response = await callHandler(
+      { video_id: videoId },
+      { user_id: userId }
+    );
+    expect(response.statusCode).toBe(200);
+
+    response = await callHandler({ video_id: uuidv4() }, { user_id: userId });
     expect(response.statusCode).toBe(403);
     expect(response.body).toEqual({
       message: "User watching too many video streams."
