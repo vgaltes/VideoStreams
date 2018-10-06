982b6a1a1dd3b5f30087ffeed4050f17e15772b3
Vicenc Garcia-Altes <vicentredrum@gmail.com>
2018-10-06 11:03:05 +0100

    Minimum implementation of the watchStream function.

--- src/functions/user/watchStream.js
+++ src/functions/user/watchStream.js
@@ -1,7 +1,11 @@
+const uuidv4 = require("uuid/v4");
+
 module.exports.handler = (event, context, callback) => {
+  const streamId = uuidv4();
+
   const response = {
     statusCode: 200,
-    body: JSON.stringify({ message: `OK User ${event.pathParameters.user_id}` })
+    body: JSON.stringify({ video_id: event.body.video_id, stream_id: streamId })
   };
   callback(null, response);
 };
